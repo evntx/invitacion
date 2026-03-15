dfce89b71cc514c977ee38922ff62d4f3e9f2dab
evntx <grisma.3d@gmail.com>
2026-03-14 20:19:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,21 @@ $(document).ready(function() {
     var envelope = $('#envelope');
     var btn_open = $("#open");
     var btn_reset = $("#reset");
+    var music = document.getElementById("musica");
 
-    envelope.click(function() {
-        openEnvelope();
-    });
-
-    btn_open.click(function() {
-        openEnvelope();
-    });
-
-    btn_reset.click(function() {
-        closeEnvelope();
-    });
-
-    function openEnvelope() {
+    function open() {
         envelope.addClass("open").removeClass("close");
+        music.play().catch(function(error) {
+            console.log("El navegador bloqueó el inicio automático hasta que el usuario interactúe.");
+        });
     }
 
-    function closeEnvelope() {
+    function close() {
         envelope.addClass("close").removeClass("open");
+        music.pause();
     }
+
+    envelope.click(open);
+    btn_open.click(open);
+    btn_reset.click(close);
 });
